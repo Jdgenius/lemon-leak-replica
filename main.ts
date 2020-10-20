c5ad7216ed7ba18707ec578c57992e410cb9f610
Jdgenius <73201567+Jdgenius@users.noreply.github.com>
2020-10-20 18:57:33 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
+scene.setBackgroundColor(6)
+let playerImg = sprites.castle.heroWalkFront1
+let projectileImg1 = sprites.castle.rock0
+let projectileImg2 = sprites.dungeon.collectibleBlueCrystal
+let itemImg = sprites.builtin.coin0
 
+//Create princess character
+let hero = sprites.create(playerImg, SpriteKind.Player)
+controller.moveSprite(hero, 100, 100)
+hero.setFlag(SpriteFlag.StayInScreen, true)
+
+game.onUpdateInterval(500, function() {
+    let projectile = sprites.createProjectileFromSide(projectileImg1, 50, 50)
+})

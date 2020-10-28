24b129df7ceda54b30e5e70fed02976f2e3713f7
Jdgenius <73201567+Jdgenius@users.noreply.github.com>
2020-10-28 18:59:19 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,48 @@
+let projectile: Sprite = null
+let projectile_2: Sprite = null
 scene.setBackgroundColor(6)
-let playerImg = sprites.castle.heroWalkFront1
-let projectileImg1 = sprites.castle.rock0
-let projectileImg2 = sprites.dungeon.collectibleBlueCrystal
+let playerImg = sprites.food.smallLemon
+let projectileImg1 = sprites.food.smallStrawberry
+let projectileImg2 = sprites.food.smallCherries
 let itemImg = sprites.builtin.coin0
 
-//Create princess character
+//Set lives
+info.setLife(100)
+
+
+// Create lemon character
 let hero = sprites.create(playerImg, SpriteKind.Player)
 controller.moveSprite(hero, 100, 100)
 hero.setFlag(SpriteFlag.StayInScreen, true)
+game.onUpdateInterval(350, function () {
+    projectile_2 = sprites.createProjectileFromSide(projectileImg2, 50, 0)
+    projectile_2.setPosition(0, randint(0, 120))
+})
+game.onUpdateInterval(200, function () {
+    projectile = sprites.createProjectileFromSide(projectileImg1, 0, 50)
+    projectile.setPosition(randint(0, 160), 0)
+})
+namespace SpriteKind {
+    export const Money = SpriteKind.create()
+}
+let item = sprites.create(itemImg, SpriteKind.Money)
+item.setPosition(randint(10, 150), randint(10, 110))
 
-game.onUpdateInterval(500, function() {
-    let projectile = sprites.createProjectileFromSide(projectileImg1, 50, 50)
+// Detect when player and money collide
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Money, function(sprite: Sprite, otherSprite: Sprite) {
+    item.setPosition(randint(10, 150), randint(10, 110))
+    info.changeScoreBy(1)
+    info.changeLifeBy(75)
+})
+
+//Detect when player and projectiles collide
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function(sprite: Sprite, otherSprite: Sprite) {
+   info.changeLifeBy(-3) 
+   
+   hero.startEffect(effects.spray,200)
+})
+game.onUpdate(function() {
+    if (info.score() == 100){
+        game.over(true)
+    }
 })
